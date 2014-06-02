18c2d8c31907ac8051726c84ed7c9c622330a3dc
Alex Fishbeck <alex@Alexs-MacBook-Pro.local>
2014-06-02 01:01:00 -0500

    Initial commit from local git

---
+++ RCM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RCM
+//
+//  Created by Alex Fishbeck on 6/2/14.
+//  Copyright (c) 2014 Rolling Cheese Man. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
